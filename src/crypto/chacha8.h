d08b390c5aebda0d12a7a6b8d568f81fa81409cd
andre <dirtybitsofficial@gmail.com>
2018-05-09 02:17:34 -0500

    Update chacha8.h

--- src/crypto/chacha8.h
+++ src/crypto/chacha8.h
@@ -9,45 +9,52 @@
 #if defined(__cplusplus)
 #include <memory.h>
 #include <string>
+#include "hash.hpp"
+#include "crypto-util.h"
 
-#include "hash.h"
-
-namespace Crypto {
-  extern "C" {
+namespace crypto {
+extern "C" {
 #endif
-    void chacha8(const void* data, size_t length, const uint8_t* key, const uint8_t* iv, char* cipher);
+
+void chacha8(const void* data, size_t length, const uint8_t* key, const uint8_t* iv, char* cipher);
+
 #if defined(__cplusplus)
-  }
+}
 
 #pragma pack(push, 1)
-  struct chacha8_key {
-    uint8_t data[CHACHA8_KEY_SIZE];
+struct chacha8_key {
+	uint8_t data[CHACHA8_KEY_SIZE];
 
-    ~chacha8_key()
-    {
-      memset(data, 0, sizeof(data));
-    }
-  };
-
-  // MS VC 2012 doesn't interpret `class chacha8_iv` as POD in spite of [9.0.10], so it is a struct
-  struct chacha8_iv {
-    uint8_t data[CHACHA8_IV_SIZE];
-  };
+	chacha8_key():data{} {}
+	explicit chacha8_key(const Hash & ha){
+		memcpy(data, ha.data, CHACHA8_KEY_SIZE); // safe because of static_assert below
+	}
+	~chacha8_key() {
+		sodium_memzero(data, sizeof(data));
+	}
+};
+struct chacha8_iv {
+	uint8_t data[CHACHA8_IV_SIZE]{};
+};
 #pragma pack(pop)
+	
+static_assert(sizeof(chacha8_key) == CHACHA8_KEY_SIZE && sizeof(chacha8_iv) == CHACHA8_IV_SIZE, "Invalid structure size");
+static_assert(sizeof(chacha8_key) <= sizeof(Hash), "Size of hash must be at least that of chacha8_key");
 
-  static_assert(sizeof(chacha8_key) == CHACHA8_KEY_SIZE && sizeof(chacha8_iv) == CHACHA8_IV_SIZE, "Invalid structure size");
-
-  inline void chacha8(const void* data, size_t length, const chacha8_key& key, const chacha8_iv& iv, char* cipher) {
-    chacha8(data, length, reinterpret_cast<const uint8_t*>(&key), reinterpret_cast<const uint8_t*>(&iv), cipher);
-  }
-
-  inline void generate_chacha8_key(Crypto::cn_context &context, const std::string& password, chacha8_key& key) {
-    static_assert(sizeof(chacha8_key) <= sizeof(Hash), "Size of hash must be at least that of chacha8_key");
-    Hash pwd_hash;
-    cn_slow_hash(context, password.data(), password.size(), pwd_hash);
-    memcpy(&key, &pwd_hash, sizeof(key));
-    memset(&pwd_hash, 0, sizeof(pwd_hash));
-  }
+inline void chacha8(const void* data, size_t length, const chacha8_key& key, const chacha8_iv& iv, void * cipher) {
+	chacha8(data, length, key.data, iv.data, (char *)cipher);
 }
+
+inline chacha8_key generate_chacha8_key(crypto::CryptoNightContext &context, const void * password_data, size_t password_size) {
+	Hash pwd_hash = context.cn_slow_hash(password_data, password_size);
+	return chacha8_key{pwd_hash};
+}
+inline chacha8_key generate_chacha8_key(crypto::CryptoNightContext &context, const std::string& password) {
+	return generate_chacha8_key(context, password.data(), password.size());
+}
+
+}
+
+//CRYPTO_MAKE_COMPARABLE(crypto, chacha8_key, crypto::sodium_compare)
 
 #endif
