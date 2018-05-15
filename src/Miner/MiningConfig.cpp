31d4288215b8f892ac64d39d87f946c4ac1188e2
andre <dirtybitsofficial@gmail.com>
2018-05-15 02:48:11 -0500

    Update MiningConfig.cpp

--- src/Miner/MiningConfig.cpp
+++ src/Miner/MiningConfig.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2012-2018, The CryptoNote developers, The Bytecoin developers.
+// Copyright (c) 2018, The Catalyst project.
 // Licensed under the GNU Lesser General Public License. See LICENSE for details.
 
 #include "MiningConfig.hpp"
@@ -11,24 +12,24 @@
 #include "CryptoNoteConfig.hpp"
 #include "logging/ILogger.hpp"
 
-using namespace bytecoin;
+using namespace catalyst;
 
 MiningConfig::MiningConfig(common::CommandLine &cmd)
-    : bytecoind_ip("127.0.0.1"), bytecoind_port(RPC_DEFAULT_PORT), thread_count(std::thread::hardware_concurrency()) {
+    : catalystd_ip("127.0.0.1"), catalystd_port(RPC_DEFAULT_PORT), thread_count(std::thread::hardware_concurrency()) {
 	if (const char *pa = cmd.get("--address"))
 		mining_address = pa;
-	if (const char *pa = cmd.get("--bytecoind-address")) {
-		if (!common::parse_ip_address_and_port(pa, bytecoind_ip, bytecoind_port))
+	if (const char *pa = cmd.get("--catalystd-address")) {
+		if (!common::parse_ip_address_and_port(pa, catalystd_ip, catalystd_port))
 			throw std::runtime_error("Wrong address format " + std::string(pa) + ", should be ip:port");
 	}
-	if (const char *pa = cmd.get("--daemon-address", "Use --bytecoind-address instead")) {
-		if (!common::parse_ip_address_and_port(pa, bytecoind_ip, bytecoind_port))
+	if (const char *pa = cmd.get("--daemon-address", "Use --catalystd-address instead")) {
+		if (!common::parse_ip_address_and_port(pa, catalystd_ip, catalystd_port))
 			throw std::runtime_error("Wrong address format " + std::string(pa) + ", should be ip:port");
 	}
-	if (const char *pa = cmd.get("--daemon-host", "Use --bytecoind-address instead"))
-		bytecoind_ip = pa;
-	if (const char *pa = cmd.get("--daemon-rpc-port", "Use --bytecoind-address instead"))
-		bytecoind_port = boost::lexical_cast<uint16_t>(pa);
+	if (const char *pa = cmd.get("--daemon-host", "Use --catalystd-address instead"))
+		catalystd_ip = pa;
+	if (const char *pa = cmd.get("--daemon-rpc-port", "Use --catalystd-address instead"))
+		catalystd_port = boost::lexical_cast<uint16_t>(pa);
 	if (const char *pa = cmd.get("--threads"))
 		thread_count = boost::lexical_cast<size_t>(pa);
 	if (const char *pa = cmd.get("--limit"))
