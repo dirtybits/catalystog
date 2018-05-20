b9634f03c2fa15908969aaffe6e1226529ea7ba9
andy <dirtybitsofficial@gmail.com>
2018-05-20 16:19:51 -0500

    Update version.hpp

--- src/version.hpp
+++ src/version.hpp
@@ -1,16 +1,17 @@
 // Copyright (c) 2012-2018, The CryptoNote developers, The Bytecoin developers.
+// Copyright (c) 2018, The Catalyst project.
 // Licensed under the GNU Lesser General Public License. See LICENSE for details.
 
 #pragma once
 
 // defines are for Windows resource compiler
-#define bytecoin_VERSION_WINDOWS_COMMA 3, 18, 5, 9
-#define bytecoin_VERSION_STRING "3.0.4"
+#define catalyst_VERSION_WINDOWS_COMMA 3, 18, 5, 9
+#define catalyst_VERSION_STRING "3.0.4"
 
 #ifndef RC_INVOKED  // Windows resource compiler
 
-namespace bytecoin {
-inline const char *app_version() { return bytecoin_VERSION_STRING; }
+namespace catalyst {
+inline const char *app_version() { return catalyst_VERSION_STRING; }
 }
 
 #endif
