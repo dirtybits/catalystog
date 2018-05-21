7e1a4c6c32b491cd447e836f6b676d8518621c81
andy <dirtybitsofficial@gmail.com>
2018-05-21 00:31:30 -0500

    Update LevinProtocol.cpp

--- src/p2p/LevinProtocol.cpp
+++ src/p2p/LevinProtocol.cpp
@@ -1,9 +1,10 @@
 // Copyright (c) 2012-2018, The CryptoNote developers, The Bytecoin developers.
+// Copyright (c) 2018, The Catalyst project.
 // Licensed under the GNU Lesser General Public License. See LICENSE for details.
 
 #include "LevinProtocol.hpp"
 
-using namespace bytecoin;
+using namespace catalyst;
 
 namespace {
 
