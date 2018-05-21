0de7316fa33b244c892f9c884fba5a59e07b605a
andy <dirtybitsofficial@gmail.com>
2018-05-21 00:44:55 -0500

    Update P2pProtocolDefinitions.hpp

--- src/p2p/P2pProtocolDefinitions.hpp
+++ src/p2p/P2pProtocolDefinitions.hpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2012-2018, The CryptoNote developers, The Bytecoin developers.
+// Copyright (c) 2018, The Catalyst project.
 // Licensed under the GNU Lesser General Public License. See LICENSE for details.
 
 #pragma once
@@ -8,7 +9,7 @@
 
 #include "crypto/crypto.hpp"
 
-namespace bytecoin {
+namespace catalyst {
 
 struct network_config {
 	uint32_t connections_count       = 0;
@@ -150,20 +151,20 @@ struct COMMAND_REQUEST_PEER_ID  // TODO - remove this message in next hard fork,
 #endif
 }
 namespace seria {
-void ser_members(bytecoin::network_config &v, seria::ISeria &s);
-void ser_members(bytecoin::basic_node_data &v, seria::ISeria &s);
-void ser_members(bytecoin::CORE_SYNC_DATA &v, seria::ISeria &s);
-void ser_members(bytecoin::COMMAND_HANDSHAKE::request &v, seria::ISeria &s);
-void ser_members(bytecoin::COMMAND_HANDSHAKE::response &v, seria::ISeria &s);
-void ser_members(bytecoin::COMMAND_TIMED_SYNC::request &v, seria::ISeria &s);
-void ser_members(bytecoin::COMMAND_TIMED_SYNC::response &v, seria::ISeria &s);
-void ser_members(bytecoin::COMMAND_PING::request &v, seria::ISeria &s);
-void ser_members(bytecoin::COMMAND_PING::response &v, seria::ISeria &s);
+void ser_members(catalyst::network_config &v, seria::ISeria &s);
+void ser_members(catalyst::basic_node_data &v, seria::ISeria &s);
+void ser_members(catalyst::CORE_SYNC_DATA &v, seria::ISeria &s);
+void ser_members(catalyst::COMMAND_HANDSHAKE::request &v, seria::ISeria &s);
+void ser_members(catalyst::COMMAND_HANDSHAKE::response &v, seria::ISeria &s);
+void ser_members(catalyst::COMMAND_TIMED_SYNC::request &v, seria::ISeria &s);
+void ser_members(catalyst::COMMAND_TIMED_SYNC::response &v, seria::ISeria &s);
+void ser_members(catalyst::COMMAND_PING::request &v, seria::ISeria &s);
+void ser_members(catalyst::COMMAND_PING::response &v, seria::ISeria &s);
 #if bytecoin_ALLOW_DEBUG_COMMANDS
-void ser_members(bytecoin::proof_of_trust &v, seria::ISeria &s);
-void ser_members(bytecoin::COMMAND_REQUEST_STAT_INFO::request &v, seria::ISeria &s);
-void ser_members(bytecoin::COMMAND_REQUEST_STAT_INFO::response &v, seria::ISeria &s);
-void ser_members(bytecoin::COMMAND_REQUEST_NETWORK_STATE::request &v, seria::ISeria &s);
-void ser_members(bytecoin::COMMAND_REQUEST_NETWORK_STATE::response &v, seria::ISeria &s);
+void ser_members(catalyst::proof_of_trust &v, seria::ISeria &s);
+void ser_members(catalyst::COMMAND_REQUEST_STAT_INFO::request &v, seria::ISeria &s);
+void ser_members(catalyst::COMMAND_REQUEST_STAT_INFO::response &v, seria::ISeria &s);
+void ser_members(catalyst::COMMAND_REQUEST_NETWORK_STATE::request &v, seria::ISeria &s);
+void ser_members(catalyst::COMMAND_REQUEST_NETWORK_STATE::response &v, seria::ISeria &s);
 #endif
 }
