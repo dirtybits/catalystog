5254962195e4ca8c59f5c893444ac42e3248067e
Andre Sustic <dirtybitsofficial@gmail.com>
2018-11-20 01:01:49 -0600

    Update CryptoNoteConfig.hpp

--- src/CryptoNoteConfig.hpp
+++ src/CryptoNoteConfig.hpp
@@ -118,7 +118,7 @@ struct CheckpointData {
 	uint32_t index;
 	const char *block_id;
 };
-// You may add here other checkpoints using the following format:
+// Add other checkpoints here using the following format:
 // {<block height>, "<block hash>"},
 constexpr const CheckpointData CHECKPOINTS[] = {
  //{ 10000, "84b6345731e2702cdaadc6ce5e5238c4ca5ecf48e3447136b2ed829b8a95f3ad" },
