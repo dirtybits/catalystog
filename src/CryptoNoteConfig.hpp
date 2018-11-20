417be387cc65778b3a7f4c3824f17a2aa8a69cbb
Andre Sustic <dirtybitsofficial@gmail.com>
2018-11-20 01:02:28 -0600

    Update CryptoNoteConfig.hpp

--- src/CryptoNoteConfig.hpp
+++ src/CryptoNoteConfig.hpp
@@ -17,7 +17,7 @@ namespace parameters {
 const uint32_t CRYPTONOTE_MAX_BLOCK_NUMBER             = 500000000;
 const uint32_t CRYPTONOTE_MAX_BLOCK_BLOB_SIZE          = 500000000;
 const uint32_t CRYPTONOTE_MAX_TX_SIZE                  = 1000000000;
-const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 0xd7;  // addresses start with "c"
+const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 0x171f54;  // addresses start with "c"
 const uint32_t CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW    = 60;
 const uint32_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT      = 60 * 60 * 2;
 
