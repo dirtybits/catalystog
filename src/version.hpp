8464a5a0f8d70b01d628dead79e70f1df6b52841
Andre Sustic <dirtybitsofficial@gmail.com>
2018-11-20 21:01:51 -0600

    Update version.hpp

--- src/version.hpp
+++ src/version.hpp
@@ -6,7 +6,7 @@
 
 // defines are for Windows resource compiler
 #define catalyst_VERSION_WINDOWS_COMMA 3, 18, 5, 24
-#define catalyst_VERSION_STRING "3.1.1" // we will need to change this for the first release
+#define catalyst_VERSION_STRING "0.0.1" // we will need to change this for the first release
 
 #ifndef RC_INVOKED  // Windows resource compiler
 
